f5e97fe486ab7786edf7572dde3192da38d91bfe
parm <pardhi1400@gmail.com>
2024-12-10 22:19:06 -0800

    restructured program to compile client and server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Commandline-Messaging-System
 Commandline Messaging System written in C that has TCP sockets with TLS encryption via openSSL.
+
---
+++ Server/server.c
@@ -0,0 +1,51 @@
+// scanf and printf
+#include <stdio.h>
+// bool
+#include <stdbool.h>
+#include <pthread.h>
+// sleep()
+#include <unistd.h>
+
+#define NUMTHREADS 2
+
+// void *readInput(void * args){
+//         printf("thread readInput");
+//         // timeout 2 seconds
+//         sleep(2);
+
+//         // Exit the thread
+//         pthread_exit(NULL);
+//     }
+    
+//     void *receiveData(void* args){
+//         printf("thread receiveData");
+//         // timeout 2 seconds
+//         sleep(2);
+//         // Exit the thread
+//         pthread_exit(NULL);
+//     }
+
+
+void* thread1(){
+    printf("Thread start\n");
+    sleep(5);
+    printf("Thread half\n");
+    sleep(5);
+    printf("Thread finish\n");
+    pthread_exit(0);
+}
+
+int main (){
+    printf("Enter IP address and port number of the client\n");
+    printf("IP address is () on port ()\n");
+    printf("Connection has been Established\n");
+    
+    // bool exit = false;
+    
+    pthread_t thread;
+    printf("Creating thread\n");
+    
+    pthread_create(&thread,NULL,thread1,NULL);
+    
+    scanf("Enter to end\n");
+}
