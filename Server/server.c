e01331ebf1d4cd5c891e82e27025cdca2151e447
parm <pardhi1400@gmail.com>
2024-12-11 18:23:53 -0800

    separated population of address from server_init

--- Server/server.c
+++ Server/server.c
@@ -9,6 +9,13 @@
 #include <sys/socket.h>
 //sock_addr_in
 #include <netinet/in.h>
+// inet_addr()
+#include <arpa/inet.h>
+// exit()
+#include <stdlib.h>
+// memset
+#include <string.h>
+
 #define NUMTHREADS 2
 
 // void *readInput(void * args){
@@ -29,26 +36,50 @@
 //     }
 
 
-void* thread1(){
+struct sockaddr_in* populateAddress(struct sockaddr_in* pAddress,socklen_t addr_size,__u_short portNumber,const char* ipAddress){
+    //initializes the address to 0
+    memset(pAddress,0,addr_size); 
+    pAddress->sin_family = AF_INET;
+    // htons converts little-endian to big-endian
+    // if i wanted this to work on all machines consider implementing a helper function that figures out byte interpretation order
+    pAddress->sin_port = htons(portNumber); 
+    // TODO: add error checking of invalid ip addresses by using inet_aton instead of inet_addr
+    pAddress->sin_addr.s_addr = inet_addr(ipAddress);
+    return pAddress;
+}
+
+// Doesnt need to be threadsafe
+int init_server(const char * ipAddress,const __u_short portNumber){
     int sockfd = socket(AF_INET,SOCK_STREAM,0);
-    // Figureout sockaddr and socklen_t
+    struct sockaddr_in address = *populateAddress(&address,sizeof(address),portNumber,ipAddress);
     int ret;
-    struct sockaddr_in address;
-    
     socklen_t addr_size = sizeof(address);
-    ret = bind(sockfd,address,addr_size);
+    ret = bind(sockfd,(struct sockaddr*)&address,addr_size);
     if(ret == -1){
+        printf("Error: Failed to bind socket");
         return -1;
     }
+    return 0;
+}
+
+void* thread1(){
+    printf("Stuff that can run asynchronusly");
     pthread_exit(0);
 }
 
 int main (){
+    int checkError;
     printf("Enter IP address and port number of the client\n");
     printf("IP address is () on port ()\n");
-    printf("Connection has been Established\n");
+    const char* ipAddress = "127.0.0.1";
+    __u_short portNumber = 3000;
+    checkError = init_server(ipAddress,portNumber);
+    if(checkError == -1){
+        printf("Error: Failed init_server()");
+        exit(-1);
+    }
     
-    // bool exit = false;
+    printf("Connection has been Established\n");
     
     pthread_t thread;
     printf("Creating thread\n");
